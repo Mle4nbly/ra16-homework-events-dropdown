a6f4ac66869f0cd46f962e78ee47d1477ce30d1f
mle4nbly <mle4nbly@gmail.com>
2025-03-26 17:20:46 +0700

    init

---
+++ src/components/DropdownList.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+const DropdownItem = ({ item, isSelected, onItem }) => {
+  return (
+    <li 
+      className={item === isSelected ? "active" : null}
+      onClick={() => onItem(item)}
+    >
+      <a href="#">{item}</a>
+    </li>
+  )
+}
+
+function DropdownList({ items }) {
+  const [isSelected, setSelected] = useState(null)
+
+  const handleItem = (item) => {
+    setSelected(item);
+
+    console.log(item);
+  }
+
+  return (
+    <ul className="dropdown">
+      {items.map((item, index) => (
+        <DropdownItem 
+          key={index} 
+          item={item}
+          isSelected={isSelected}
+          onItem={handleItem}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export default DropdownList
